b94549e253ae909c77d01df3614f556727f2aac3
Jams Huang <xjamsx@gmail.com>
2018-11-21 15:03:10 +0800

    arTest

--- webrtc/js/main.js
+++ webrtc/js/main.js
@@ -1,4 +1,3 @@
-
 const video = document.querySelector('video');
 const canvas = window.canvas = document.querySelector('canvas');
 canvas.width = 480;
@@ -15,6 +14,34 @@ const constraints = {
   audio: false,
   video: true
 };
+navigator.mediaDevices.enumerateDevices().then(function(devices) {
+  devices = devices.filter(function(devices) {
+    return devices.kind === 'videoinput';
+  });
+  var videoinput_id = '';
+  devices.forEach(function(device) {
+    if (device.label.toLowerCase().search("back") & gt; - 1) {
+      videoinput_id = device.deviceId;
+    }
+  });
+  if (videoinput_id != '') {
+    navigator.mediaDevices.getUserMedia({
+      video: {
+        deviceId: {
+          'exact': videoinput_id
+        },
+        facingMode: 'environment'
+      }
+    }).then(successCallback);
+  } else {
+    navigator.mediaDevices.getUserMedia({
+      video: {
+        facingMode: 'environment'
+      }
+    }).then(successCallback);
+  }
+
+});
 
 function handleSuccess(stream) {
   window.stream = stream; // make stream available to browser console
