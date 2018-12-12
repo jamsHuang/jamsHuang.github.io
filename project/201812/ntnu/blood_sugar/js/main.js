c443caf477170032616ec0faaed6f26e53a05e5a
JamsYC Huang <xjamsx@gmail.com>
2018-12-13 07:31:08 +0800

    fix ntnu project

--- project/201812/ntnu/blood_sugar/js/main.js
+++ project/201812/ntnu/blood_sugar/js/main.js
@@ -1,76 +1,104 @@
 $(function() {
+  var tf=-1;
+  const b0s0 = "進食時會吸收養分，血濃濃度應該會如何呢？";
+  const b1s0 = "運動時，細胞會需要很多能量，表示需要葡萄糖喔";
+  const b2s0 = "胰島素不會讓血糖濃度上升喔";
+  const b3s0 = "升糖素不會讓血糖濃度下降喔";
+  const b4s0 = "緊張時，表示細胞需要能量，會需要獲得葡萄糖喔";
+  const b5s0 = "肝糖如果分解，應該會產生葡萄糖喔";
+  const b6s0 = "肝糖主要是來自葡萄糖的合成喔";
+  const b7s0 = "細胞加速使用血糖的話，那麼，血糖濃度應該會如何呢？";
+  const b8s0 = "細胞使用血糖，會讓血糖濃度下降";
 
-  const b0s0 = "不會造成血糖下降";
-  const b1s0 = "不會造成血糖下降";
-  const b2s0 = "會造成血糖下降";
-  const b3s0 = "不會造成血糖下降";
-  const b4s0 = "不會造成血糖下降";
-  const b5s0 = "會造成血糖下降";
-  const b6s0 = "會造成血糖下降";
-  const b7s0 = "不會造成血糖下降";
+  const b0s1 = "攝食、消化與吸收，會讓血糖濃度快速地升高";
+  const b1s1 = "運動時，細胞會需要很多能量，表示需要葡萄糖喔";
+  const b2s1 = "胰島素不會讓血糖濃度上升喔";
+  const b3s1 = "升糖素不會讓血糖濃度下降喔";
+  const b4s1 = "緊張時，表示細胞需要能量，會需要獲得葡萄糖喔";
+  const b5s1 = "肝糖如果分解，應該會產生葡萄糖喔";
+  const b6s1 = "肝糖主要是來自葡萄糖的合成喔";
+  const b7s1 = "細胞加速使用血糖的話，那麼，血糖濃度應該會如何呢？";
+  const b8s1 = "細胞持續使用血糖的話，那麼，血糖濃度應該會如何呢？";
 
-  const b0s1 = "會造成血糖上升";
-  const b1s1 = "會造成血糖上升";
-  const b2s1 = "不會造成血糖上升";
-  const b3s1 = "會造成血糖上升";
-  const b4s1 = "會造成血糖上升";
-  const b5s1 = "會造成血糖上升";
-  const b6s1 = "會造成血糖上升";
-  const b7s1 = "不會造成血糖上升";
+  const b0s2 = "進食時會吸收養分，血濃濃度應該會如何呢？";
+  const b1s2 = "運動時，細胞會需要很多能量，表示需要葡萄糖喔";
+  const b2s2 = "胰島素分泌會促使血糖濃度降低";
+  const b3s2 = "升糖素不會讓血糖濃度下降喔";
+  const b4s2 = "緊張時，表示細胞需要能量，會需要獲得葡萄糖喔";
+  const b5s2 = "肝糖如果分解，應該會產生葡萄糖喔";
+  const b6s2 = "合成肝糖，表示血糖濃度會下降";
+  const b7s2 = "細胞使用血糖，會讓血糖濃度下降";
+  const b8s2 = "細胞持續使用血糖的話，那麼，血糖濃度應該會如何呢？";
 
-  const b0s2 = "不會造成血糖下降";
-  const b1s2 = "不會造成血糖下降";
-  const b2s2 = "不會造成血糖下降";
-  const b3s2 = "會造成血糖下降";
-  const b4s2 = "會造成血糖下降";
-  const b5s2 = "會造成血糖下降";
-  const b6s2 = "會造成血糖下降";
-  const b7s2 = "不會造成血糖下降";
+  const b0s3 = "進食時會吸收養分，血濃濃度應該會如何呢？";
+  const b1s3 = "運動時，細胞會需要很多能量，表示需要葡萄糖喔";
+  const b2s3 = "胰島素分泌會促使血糖濃度降低";
+  const b3s3 = "升糖素分泌會促使血糖濃度升高";
+  const b4s3 = "緊張時，表示細胞需要能量，會需要獲得葡萄糖喔";
+  const b5s3 = "肝糖如果分解，應該會產生葡萄糖喔";
+  const b6s3 = "肝糖主要是來自葡萄糖的合成喔";
+  const b7s3 = "細胞加速使用血糖的話，那麼，血糖濃度應該會如何呢？";
+  const b8s3 = "細胞持續使用血糖的話，那麼，血糖濃度應該會如何呢？";
 
-  const b0s3 = "不會造成血糖上升";
-  const b1s3 = "不會造成血糖上升";
-  const b2s3 = "不會造成血糖上升";
-  const b3s3 = "會造成血糖上升";
-  const b4s3 = "會造成血糖上升";
-  const b5s3 = "會造成血糖上升";
-  const b6s3 = "會造成血糖上升";
-  const b7s3 = "不會造成血糖上升";
+  const b0s4 = "攝食、消化與吸收，會讓血糖濃度快速地升高";
+  const b1s4 = "運動時，腎上腺素分泌量增加，血糖濃度快速升高";
+  const b2s4 = "胰島素不會讓血糖濃度上升喔";
+  const b3s4 = "升糖素不會讓血糖濃度下降喔";
+  const b4s4 = "緊張時，腎上腺素分泌量增加，血糖濃度快速升高";
+  const b5s4 = "肝糖如果分解，應該會產生葡萄糖喔";
+  const b6s4 = "肝糖主要是來自葡萄糖的合成喔";
+  const b7s4 = "細胞加速使用血糖的話，那麼，血糖濃度應該會如何呢？";
+  const b8s4 = "細胞持續使用血糖的話，那麼，血糖濃度應該會如何呢？";
 
-  const b0s4 = "會造成血糖上升";
-  const b1s4 = "不會造成血糖上升";
-  const b2s4 = "不會造成血糖上升";
-  const b3s4 = "會造成血糖上升";
-  const b4s4 = "會造成血糖上升";
-  const b5s4 = "會造成血糖上升";
-  const b6s4 = "不會造成血糖上升";
-  const b7s4 = "不會造成血糖上升";
+  const b0s5 = "進食時會吸收養分，血濃濃度應該會如何呢？";
+  const b1s5 = "運動時，細胞會需要很多能量，表示需要葡萄糖喔";
+  const b2s5 = "胰島素分泌會促使血糖濃度降低";
+  const b3s5 = "升糖素不會讓血糖濃度下降喔";
+  const b4s5 = "緊張時，表示細胞需要能量，會需要獲得葡萄糖喔";
+  const b5s5 = "肝糖如果分解，應該會產生葡萄糖喔";
+  const b6s5 = "合成肝糖，表示血糖濃度會下降";
+  const b7s5 = "細胞使用血糖，會讓血糖濃度下降";
+  const b8s5 = "細胞持續使用血糖的話，那麼，血糖濃度應該會如何呢？";
 
-  const b0s5 = "不會造成血糖上升";
-  const b1s5 = "不會造成血糖上升";
-  const b2s5 = "不會造成血糖上升";
-  const b3s5 = "會造成血糖上升";
-  const b4s5 = "會造成血糖上升";
-  const b5s5 = "會造成血糖上升";
-  const b6s5 = "會造成血糖上升";
-  const b7s5 = "不會造成血糖上升";
+  const b0s6 = "進食時會吸收養分，血濃濃度應該會如何呢？";
+  const b1s6 = "運動時，細胞會需要很多能量，表示需要葡萄糖喔";
+  const b2s6 = "胰島素分泌會促使血糖濃度降低";
+  const b3s6 = "升糖素分泌會促使血糖濃度升高";
+  const b4s6 = "緊張時，表示細胞需要能量，會需要獲得葡萄糖喔";
+  const b5s6 = "肝糖如果分解，應該會產生葡萄糖喔";
+  const b6s6 = "肝糖主要是來自葡萄糖的合成喔";
+  const b7s6 = "細胞加速使用血糖的話，那麼，血糖濃度應該會如何呢？";
+  const b8s6 = "細胞持續使用血糖的話，那麼，血糖濃度應該會如何呢？";
 
-  const b0s6 = "不會造成血糖上升";
-  const b1s6 = "不會造成血糖上升";
-  const b2s6 = "不會造成血糖上升";
-  const b3s6 = "會造成血糖上升";
-  const b4s6 = "會造成血糖上升";
-  const b5s6 = "會造成血糖上升";
-  const b6s6 = "會造成血糖上升";
-  const b7s6 = "不會造成血糖上升";
+  const b0s7 = "攝食、消化與吸收，會讓血糖濃度快速地升高";
+  const b1s7 = "運動時，腎上腺素分泌量增加，血糖濃度快速升高";
+  const b2s7 = "胰島素不會讓血糖濃度上升喔";
+  const b3s7 = "升糖素不會讓血糖濃度下降喔";
+  const b4s7 = "緊張時，腎上腺素分泌量增加，血糖濃度快速升高";
+  const b5s7 = "肝糖如果分解，應該會產生葡萄糖喔";
+  const b6s7 = "肝糖主要是來自葡萄糖的合成喔";
+  const b7s7 = "細胞加速使用血糖的話，那麼，血糖濃度應該會如何呢？";
+  const b8s7 = "細胞持續使用血糖的話，那麼，血糖濃度應該會如何呢？";
 
-  const b0s7 = "會造成血糖上升";
-  const b1s7 = "不會造成血糖上升";
-  const b2s7 = "不會造成血糖上升";
-  const b3s7 = "會造成血糖上升";
-  const b4s7 = "會造成血糖上升";
-  const b5s7 = "會造成血糖上升";
-  const b6s7 = "不會造成血糖上升";
-  const b7s7 = "不會造成血糖上升";
+  const b0s8 = "進食時會吸收養分，血濃濃度應該會如何呢？";
+  const b1s8 = "運動時，細胞會需要很多能量，表示需要葡萄糖喔";
+  const b2s8 = "胰島素分泌會促使血糖濃度降低";
+  const b3s8 = "升糖素不會讓血糖濃度下降喔";
+  const b4s8 = "緊張時，表示細胞需要能量，會需要獲得葡萄糖喔";
+  const b5s8 = "肝糖如果分解，應該會產生葡萄糖喔";
+  const b6s8 = "合成肝糖，表示血糖濃度會下降";
+  const b7s8 = "細胞使用血糖，會讓血糖濃度下降";
+  const b8s8 = "細胞持續使用血糖的話，那麼，血糖濃度應該會如何呢？";
+
+  const b0s9 = "進食時會吸收養分，血濃濃度應該會如何呢？";
+  const b1s9 = "運動時，細胞會需要很多能量，表示需要葡萄糖喔";
+  const b2s9 = "胰島素不會讓血糖濃度上升喔";
+  const b3s9 = "升糖素不會讓血糖濃度下降喔";
+  const b4s9 = "緊張時，表示細胞需要能量，會需要獲得葡萄糖喔";
+  const b5s9 = "肝糖如果分解，應該會產生葡萄糖喔";
+  const b6s9 = "肝糖主要是來自葡萄糖的合成喔";
+  const b7s9 = "細胞加速使用血糖的話，那麼，血糖濃度應該會如何呢？";
+  const b8s9 = "細胞使用血糖，會讓血糖濃度下降";
 
   const noticeText = "請拖拉狀況按鈕"
 
@@ -140,178 +168,344 @@ $(function() {
     switch (id) {
       case 's0':
         if (dragtarget == 'block_0') {
-          str = bText + b0s0;
+          str = b0s0;
+          tf = 0;
         } else if (dragtarget == 'block_1') {
-          str = bText + b1s0;
+          str = b1s0;
+          tf = 0;
         } else if (dragtarget == 'block_2') {
-          str = bText + b2s0;
+          str = b2s0;
+          tf = 0;
         } else if (dragtarget == 'block_3') {
-          str = bText + b3s0;
+          str = b3s0;
+          tf = 0;
         } else if (dragtarget == 'block_4') {
-          str = bText + b4s0;
+          str = b4s0;
+          tf = 0;
         } else if (dragtarget == 'block_5') {
-          str = bText + b5s0;
+          str = b5s0;
+          tf = 0;
         } else if (dragtarget == 'block_6') {
-          str = bText + b6s0;
+          str = b6s0;
+          tf = 0;
         } else if (dragtarget == 'block_7') {
-          str = bText + b7s0;
+          str = b7s0;
+          tf = 0;
+        } else if (dragtarget == 'block_8') {
+          str = b8s0;
+          tf = 1;
         } else {
           str = noticeText;
+          tf = -1;
         }
         break;
       case 's1':
         if (dragtarget == 'block_0') {
-          str = bText + b0s1;
+          str = b0s1;
+          tf = 1;
         } else if (dragtarget == 'block_1') {
-          str = bText + b1s1;
+          str = b1s1;
+          tf = 0;
         } else if (dragtarget == 'block_2') {
-          str = bText + b2s1;
+          str = b2s1;
+          tf = 0;
         } else if (dragtarget == 'block_3') {
-          str = bText + b3s1;
+          str = b3s1;
+          tf = 0;
         } else if (dragtarget == 'block_4') {
-          str = bText + b4s1;
+          str = b4s1;
+          tf = 0;
         } else if (dragtarget == 'block_5') {
-          str = bText + b5s1;
+          str = b5s1;
+          tf = 0;
         } else if (dragtarget == 'block_6') {
-          str = bText + b6s1;
+          str = b6s1;
+          tf = 0;
         } else if (dragtarget == 'block_7') {
-          str = bText + b7s1;
+          str = b7s1;
+          tf = 0;
+        } else if (dragtarget == 'block_8') {
+          str = b8s1;
+          tf = 0;
         } else {
           str = noticeText;
+          tf = -1;
         }
         break;
       case 's2':
         if (dragtarget == 'block_0') {
-          str = bText + b0s2;
+          str = b0s2;
+          tf = 0;
         } else if (dragtarget == 'block_1') {
-          str = bText + b1s2;
+          str = b1s2;
+          tf = 0;
         } else if (dragtarget == 'block_2') {
-          str = bText + b2s2;
+          str = b2s2;
+          tf = 1;
         } else if (dragtarget == 'block_3') {
-          str = bText + b3s2;
+          str = b3s2;
+          tf = 0;
         } else if (dragtarget == 'block_4') {
-          str = bText + b4s2;
+          str = b4s2;
+          tf = 0;
         } else if (dragtarget == 'block_5') {
-          str = bText + b5s2;
+          str = b5s2;
+          tf = 0;
         } else if (dragtarget == 'block_6') {
-          str = bText + b6s2;
+          str = b6s2;
+          tf = 1;
         } else if (dragtarget == 'block_7') {
-          str = bText + b7s2;
+          str = b7s2;
+          tf = 1;
+        } else if (dragtarget == 'block_8') {
+          str = b8s2;
+          tf = 0;
         } else {
           str = noticeText;
+          tf = -1;
         }
         break;
       case 's3':
         if (dragtarget == 'block_0') {
-          str = bText + b0s3;
+          str = b0s3;
+          tf = 0;
         } else if (dragtarget == 'block_1') {
-          str = bText + b1s3;
+          str = b1s3;
+          tf = 0;
         } else if (dragtarget == 'block_2') {
-          str = bText + b2s3;
+          str = b2s3;
+
+          tf = 1;
         } else if (dragtarget == 'block_3') {
-          str = bText + b3s3;
+          str = b3s3;
+          tf = 1;
         } else if (dragtarget == 'block_4') {
-          str = bText + b4s3;
+          str = b4s3;
+          tf = 0;
         } else if (dragtarget == 'block_5') {
-          str = bText + b5s3;
+          str = b5s3;
+          tf = 0;
         } else if (dragtarget == 'block_6') {
-          str = bText + b6s3;
+          str = b6s3;
+          tf = 0;
         } else if (dragtarget == 'block_7') {
-          str = bText + b7s3;
+          str = b7s3;
+          tf = 0;
+        } else if (dragtarget == 'block_8') {
+          str = b8s3;
+          tf = 0;
         } else {
           str = noticeText;
+          tf = -1;
         }
         break;
       case 's4':
         if (dragtarget == 'block_0') {
-          str = bText + b0s4;
+          str = b0s4;
+          tf = 1;
         } else if (dragtarget == 'block_1') {
-          str = bText + b1s4;
+          str = b1s4;
+          tf = 1;
         } else if (dragtarget == 'block_2') {
-          str = bText + b2s4;
+          str = b2s4;
+          tf = 0;
         } else if (dragtarget == 'block_3') {
-          str = bText + b3s4;
+          str = b3s4;
+          tf = 0;
         } else if (dragtarget == 'block_4') {
-          str = bText + b4s4;
+          str = b4s4;
+          tf = 1;
         } else if (dragtarget == 'block_5') {
-          str = bText + b5s4;
+          str = b5s4;
+          tf = 0;
         } else if (dragtarget == 'block_6') {
-          str = bText + b6s4;
+          str = b6s4;
+          tf = 0;
         } else if (dragtarget == 'block_7') {
-          str = bText + b7s4;
-        } else {
+          str = b7s4;
+          tf = 0;
+        }  else if (dragtarget == 'block_8') {
+          str = b8s4;
+          tf = 0;
+        }else {
           str = noticeText;
+          tf = -1;
         }
         break;
       case 's5':
         if (dragtarget == 'block_0') {
-          str = bText + b0s5;
+          str = b0s5;
+          tf = 0;
         } else if (dragtarget == 'block_1') {
-          str = bText + b1s5;
+          str = b1s5;
+          tf = 0;
         } else if (dragtarget == 'block_2') {
-          str = bText + b2s5;
+          str = b2s5;
+          tf = 1;
         } else if (dragtarget == 'block_3') {
-          str = bText + b3s5;
+          str = b3s5;
+          tf = 0;
         } else if (dragtarget == 'block_4') {
-          str = bText + b4s5;
+          str = b4s5;
+          tf = 0;
         } else if (dragtarget == 'block_5') {
-          str = bText + b5s5;
+          str = b5s5;
+          tf = 0;
         } else if (dragtarget == 'block_6') {
-          str = bText + b6s5;
+          str = b6s5;
+          tf = 1;
         } else if (dragtarget == 'block_7') {
-          str = bText + b7s5;
-        } else {
+          str = b7s5;
+          tf = 1;
+        }  else if (dragtarget == 'block_8') {
+          str = b8s5;
+          tf = 0;
+        }else {
           str = noticeText;
+          tf = -1;
         }
         break;
       case 's6':
         if (dragtarget == 'block_0') {
-          str = bText + b0s6;
+          str = b0s6;
+          tf = 0;
         } else if (dragtarget == 'block_1') {
-          str = bText + b1s6;
+          str = b1s6;
+          tf = 0;
         } else if (dragtarget == 'block_2') {
-          str = bText + b2s6;
+          str = b2s6;
+          tf = 1;
         } else if (dragtarget == 'block_3') {
-          str = bText + b3s6;
+          str = b3s6;
+          tf = 1;
         } else if (dragtarget == 'block_4') {
-          str = bText + b4s6;
+          str = b4s6;
+          tf = 0;
         } else if (dragtarget == 'block_5') {
-          str = bText + b5s6;
+          str = b5s6;
+          tf = 0;
         } else if (dragtarget == 'block_6') {
-          str = bText + b6s6;
+          str = b6s6;
+          tf = 0;
         } else if (dragtarget == 'block_7') {
-          str = bText + b7s6;
-        } else {
+          str = b7s6;
+          tf = 0;
+        }  else if (dragtarget == 'block_8') {
+          str = b8s6;
+          tf = 0;
+        }else {
           str = noticeText;
+          tf = -1;
         }
         break;
       case 's7':
         if (dragtarget == 'block_0') {
-          str = bText + b0s7;
+          str = b0s7;
+          tf = 1;
         } else if (dragtarget == 'block_1') {
-          str = bText + b1s7;
+          str = b1s7;
+          tf = 1;
         } else if (dragtarget == 'block_2') {
-          str = bText + b2s7;
+          str = b2s7;
+          tf = 0;
         } else if (dragtarget == 'block_3') {
-          str = bText + b3s7;
+          str = b3s7;
+          tf = 0;
         } else if (dragtarget == 'block_4') {
-          str = bText + b4s7;
+          str = b4s7;
+          tf = 1;
         } else if (dragtarget == 'block_5') {
-          str = bText + b5s7;
+          str = b5s7;
+          tf = 0;
         } else if (dragtarget == 'block_6') {
-          str = bText + b6s7;
+          str = b6s7;
+          tf = 0;
         } else if (dragtarget == 'block_7') {
-          str = bText + b7s7;
-        } else {
+          str = b7s7;
+          tf = 0;
+        }  else if (dragtarget == 'block_8') {
+          str = b8s7;
+          tf = 0;
+        }else {
           str = noticeText;
+          tf = -1;
         }
         break;
+        case 's8':
+          if (dragtarget == 'block_0') {
+            str = b0s8;
+            tf = 0;
+          } else if (dragtarget == 'block_1') {
+            str = b1s8;
+            tf = 0;
+          } else if (dragtarget == 'block_2') {
+            str = b2s8;
+            tf = 1;
+          } else if (dragtarget == 'block_3') {
+            str = b3s8;
+            tf = 0;
+          } else if (dragtarget == 'block_4') {
+            str = b4s8;
+            tf = 0;
+          } else if (dragtarget == 'block_5') {
+            str = b5s8;
+            tf = 0;
+          } else if (dragtarget == 'block_6') {
+            str = b6s8;
+            tf = 1;
+          } else if (dragtarget == 'block_7') {
+            str = b7s8;
+            tf = 1;
+          } else if (dragtarget == 'block_8') {
+            str = b8s8;
+            tf = 0;
+          } else {
+            str = noticeText;
+            tf = -1;
+          }
+          break;
+          case 's9':
+            if (dragtarget == 'block_0') {
+              str = b0s9;
+              tf = 0;
+            } else if (dragtarget == 'block_1') {
+              str = b1s9;
+              tf = 0;
+            } else if (dragtarget == 'block_2') {
+              str = b2s9;
+              tf = 0;
+            } else if (dragtarget == 'block_3') {
+              str = b3s9;
+              tf = 0;
+            } else if (dragtarget == 'block_4') {
+              str = b4s9;
+              tf = 0;
+            } else if (dragtarget == 'block_5') {
+              str = b5s9;
+              tf = 0;
+            } else if (dragtarget == 'block_6') {
+              str = b6s9;
+              tf = 0;
+            } else if (dragtarget == 'block_7') {
+              str = b7s9;
+              tf = 0;
+            } else if (dragtarget == 'block_8') {
+              str = b8s9;
+              tf = 1;
+            } else {
+              str = noticeText;
+              tf = -1;
+            }
+            break;
     }
     $('.notice_box_text').html(str);
-    if (str.indexOf('不') == -1) {
+    if (tf==1) {
       $('.notice_box').css('background-color', '#c8eac8')
-    } else {
+    } else if(tf==0){
       $('.notice_box').css('background-color', '#f39696')
+    }
+    else{
+      $('.notice_box').css('background-color', '#ACACAC')
     }
   }
 
--- project/201812/ntnu/breathe/js/main.js
+++ project/201812/ntnu/breathe/js/main.js
@@ -17,22 +17,27 @@ $(function() {
   var inhale = false;
   var exhale = false;
   //
+  var showImg = 0;
+  //
   var pass_frame;
+
   function init() {
     setupSlider();
     switchImg();
     runtime(now_frame);
-    $('.button_box_breathe').css('background-color','#ACACAC');
+    $('.button_box_breathe').css('background-color', '#ACACAC');
 
-    $('.button_box_breathe').click(function(){
+    $('.button_box_breathe').click(function() {
+      showImg = 0;
+      $('.show_img').css({opacity:0.5});
+      $('.all_img').css({opacity:1});
       resetBtn();
-      if(breathe == true){
+      if (breathe == true) {
         breathe = false;
         inhale = false;
         exhale = false;
-      }
-      else{
-        $('.button_box_breathe').css('background-color','#ACACAC');
+      } else {
+        $('.button_box_breathe').css('background-color', '#ACACAC');
         breathe = true;
         inhale = false;
         exhale = false;
@@ -40,27 +45,31 @@ $(function() {
       }
       //runtime(now_frame);
     })
-    $('.button_box_inhale').click(function(){
+    $('.button_box_inhale').click(function() {
+      showImg = 0;
+      $('.show_img').css({opacity:0.5});
+      $('.all_img').css({opacity:1});
       resetBtn();
-      if(inhale == true){
+      if (inhale == true) {
         breathe = false;
         inhale = false;
         exhale = false;
-      }else{
-          $('.button_box_inhale').css('background-color','#ACACAC');
+      } else {
+        $('.button_box_inhale').css('background-color', '#ACACAC');
         breathe = false;
         inhale = true;
         exhale = false;
         num = 0;
       }
     })
-    $('.button_box_exhale').click(function(){
+    $('.button_box_exhale').click(function() {
+      showImg = 0;
+      $('.show_img').css({opacity:0.5});
       resetBtn();
-      if(exhale == true){
+      if (exhale == true) {
         exhale = false;
-      }
-      else{
-          $('.button_box_exhale').css('background-color','#ACACAC');
+      } else {
+        $('.button_box_exhale').css('background-color', '#ACACAC');
         breathe = false;
         inhale = false;
         exhale = true;
@@ -74,40 +83,34 @@ $(function() {
     now_frame = Math.round(num);
 
     //console.log(breathe,inhale,exhale);
-    if(breathe){
+    if (breathe) {
 
-      if(num>(total_frames-1) || num<0 )
-      {
-        runSpd*=-1;
-      }
-      else{
+      if (num > (total_frames - 1) || num < 0) {
+        runSpd *= -1;
+      } else {
 
       }
-    }else if(inhale){
-      if(num>(total_frames-1))
-      {
-        num = total_frames-1;
+    } else if (inhale) {
+      if (num > (total_frames - 1)) {
+        num = total_frames - 1;
         runSpd = 0;
         inhale = false;
         resetBtn();
-      }
-      else {
+      } else {
 
         runSpd = spd;
       }
-    }
-    else if(exhale){
-      if(num<0){
+    } else if (exhale) {
+      if (num < 0) {
         num = 0;
         runSpd = 0;
         exhale = false;
         resetBtn();
-      }else{
+      } else {
 
         runSpd = -spd;
       }
-    }
-    else{
+    } else {
       runSpd = 0;
       //resetBtn();
     }
@@ -115,15 +118,19 @@ $(function() {
     switchImg();
     requestAnimationFrame(runtime);
   }
-  function resetBtn(){
-    $('.button_box_breathe').css('background-color','#FFFFFF');
-    $('.button_box_inhale').css('background-color','#FFFFFF');
-    $('.button_box_exhale').css('background-color','#FFFFFF');
+
+  function resetBtn() {
+    $('.button_box_breathe').css('background-color', '#FFFFFF');
+    $('.button_box_inhale').css('background-color', '#FFFFFF');
+    $('.button_box_exhale').css('background-color', '#FFFFFF');
   }
 
   function setupSlider() {
     bone_slider = $("#bones_ctrl").slider();
     $("#bones_ctrl").on("slide", function(slideEvt) {
+      $('.show_img').css({opacity:1});
+      $('.all_img').css({opacity:0.5});
+      showImg = 1;
       breathe = false;
       inhale = false;
       exhale = false;
@@ -147,6 +154,9 @@ $(function() {
     });
     yokohama_slider = $("#yokohama_ctrl").slider();
     $("#yokohama_ctrl").on("slide", function(slideEvt) {
+      $('.show_img').css({opacity:1});
+      $('.all_img').css({opacity:0.5});
+      showImg = 2;
       breathe = false;
       inhale = false;
       exhale = false;
@@ -159,21 +169,49 @@ $(function() {
 
   function switchImg() {
 
-    if(now_frame == pass_frame){
+    if (now_frame == pass_frame) {
 
-    }else{
+    } else {
       var urlCreator = window.URL || window.webkitURL;
-      var boneUrl = urlCreator.createObjectURL(preload.getResult("fb" + now_frame, true));
-      var lungUrl = urlCreator.createObjectURL(preload.getResult("fl" + now_frame, true));
+      var front_all_Url = urlCreator.createObjectURL(preload.getResult("fa" + now_frame, true));
+      var side_all_Url = urlCreator.createObjectURL(preload.getResult("sa" + now_frame, true));
       // var smokeUrl = urlCreator.createObjectURL(preload.getResult("fs" + now_frame, true));
-      $('.front_lung_img').attr("src", lungUrl);
-      $('.front_bone_img').attr("src", boneUrl);
+      var show_url_front;
+      var show_url_side;
+      if (showImg == 0) {
+        show_url_front = urlCreator.createObjectURL(preload.getResult("fs" + now_frame, true));
+        show_url_side = urlCreator.createObjectURL(preload.getResult("ss" + now_frame, true));
+      } else if (showImg == 1) {
+        show_url_front = urlCreator.createObjectURL(preload.getResult("fb" + now_frame, true));
+        show_url_side = urlCreator.createObjectURL(preload.getResult("sb" + now_frame, true));
+      } else if (showImg == 2) {
+        show_url_front = urlCreator.createObjectURL(preload.getResult("fd" + now_frame, true));
+        show_url_side = urlCreator.createObjectURL(preload.getResult("sd" + now_frame, true));
+      } else if (showImg == 3) {
+        show_url_front = urlCreator.createObjectURL(preload.getResult("fl" + now_frame, true));
+        show_url_side = urlCreator.createObjectURL(preload.getResult("sl" + now_frame, true));
+      }
+      $('.front_all_img').attr("src", front_all_Url);
+      $('.side_all_img').attr("src", side_all_Url);
       // $('.front_smoke_img').attr("src", smokeUrl);
+      $('.front_show_img').attr("src", show_url_front);
+      $('.side_show_img').attr("src", show_url_side);
+      //
       yokohama_slider.slider('setValue', now_frame);
       bone_slider.slider('setValue', now_frame);
       lung_time = now_frame;
       //console.log(lung_time);
-      setTimeout(function(){ lung_slider.slider('setValue',lung_time); }, 100);
+      if (pass_frame < now_frame) {
+        $('.textbox_text').html("胸腔變大");
+      } else if (pass_frame > now_frame) {
+        $('.textbox_text').html("胸腔縮小");
+      } else {
+        $('.textbox_text').html("");
+      }
+
+      setTimeout(function() {
+        lung_slider.slider('setValue', lung_time);
+      }, 100);
       pass_frame = now_frame
     }
 
@@ -191,21 +229,49 @@ $(function() {
 
   function setupManifest() {
     for (var i = 0; i < total_frames; i++) {
+      var faname = "fa" + i;
       var fbname = "fb" + i;
       var flname = "fl" + i;
       var fsname = "fs" + i;
+      var fdname = "fd" + i;
+      //
+      var saname = "sa" + i;
+      var sbname = "sb" + i;
+      var slname = "sl" + i;
+      var ssname = "ss" + i;
+      var sdname = "sd" + i;
+
       manifest.push({
+        id: faname,
+        src: "img/video/front_all/image__" + i + ".png",
+      }, {
         id: fbname,
         src: "img/video/front_bone/image__" + i + ".png",
       }, {
         id: flname,
         src: "img/video/front_lung/image__" + i + ".png",
-      },
-      // {
-      //   id: fsname,
-      //   src: "img/video/front_smoke/image__" + i + ".png",
-      // }
-      );
+      }, {
+        id: fdname,
+        src: "img/video/front_diaphragm/image__" + i + ".png",
+      }, {
+        id: fsname,
+        src: "img/video/front_smoke/image__" + i + ".png",
+      }, {
+        id: saname,
+        src: "img/video/side_all/image__" + i + ".png",
+      }, {
+        id: sbname,
+        src: "img/video/side_bone/image__" + i + ".png",
+      }, {
+        id: sdname,
+        src: "img/video/side_diaphragm/image__" + i + ".png",
+      }, {
+        id: slname,
+        src: "img/video/side_lung/image__" + i + ".png",
+      }, {
+        id: ssname,
+        src: "img/video/side_smoke/image__" + i + ".png",
+      }, );
     }
     //console.log(manifest);
   }
