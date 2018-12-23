0b7e736ee198faae8e8f24861f64e5d794779a81
JamsYC Huang <xjamsx@gmail.com>
2018-12-23 23:39:18 +0800

    try link

--- project/201812/xmas/js/main.js
+++ project/201812/xmas/js/main.js
@@ -32,7 +32,7 @@ $(function() {
       $('.notsupport').css("display", "flex");
       $('.notsupport').on('click',function(){
         console.log("click");
-        window.open("safari-https://different-xmas-tree.tw","_blank");
+        window.open("safari:// different-xmas-tree.tw","_blank");
       })
     }
   }
